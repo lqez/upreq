fe8e46b68290b309155a92356bccd6acfb5648c6
Park Hyunwoo <ez@smartstudy.co.kr>
2014-05-12 21:18:33 +0900

    Add support for non-pip packages

--- upreq.py
+++ upreq.py
@@ -5,40 +5,58 @@ from collections import OrderedDict
 from subprocess import Popen, PIPE
 
 parser = argparse.ArgumentParser(description='Update requirements.txt with pip freeze result')
-parser.add_argument('-i', dest="input", type=argparse.FileType('r'), default='requirements.txt')
-parser.add_argument('-c', dest="command", default='pip freeze')
-parser.add_argument('-o', dest="output", type=argparse.FileType('w', 0), default='requirements.txt')
+parser.add_argument('-i', dest='input', default='requirements.txt')
+parser.add_argument('-c', dest='command', default='pip freeze')
+parser.add_argument('-o', dest='output', default='requirements.txt')
 parser.add_argument('--add-new', dest='add_new', action='store_true')
 parser.set_defaults(add_new=False)
 
-PIP_PATTERN = r'(?P<package>\w+)==(?P<version>.+)'
+PACKAGE_RULES = {
+    'pip': {
+        'pattern': r'(?P<package>\w+)==(?P<version>.+)',
+        'output': '{package}=={version}',
+    },
+    'egg': {
+        'pattern': r'(?P<package_url>[^#]+)#egg=(?P<package>.+)',
+        'output': '{package_url}#egg={package}',
+    },
+}
 
 
 def upreq(args):
-    # 1. Read current package list from file
     packages = OrderedDict()
-    for package_info in args.input.readlines():
-        m = re.match(PIP_PATTERN, package_info)
 
-        if m:
-            packages[m.group('package')] = m.group('version')
+    # 1. Read current package list from file
+    fh = open(args.input, 'r')
+    for package_str in fh.readlines():
+        for package_type, rule in PACKAGE_RULES.iteritems():
+            m = re.match(rule['pattern'], package_str)
+            if m:
+                packages[m.group('package')] = {'type': package_type}
+                packages[m.group('package')].update(m.groupdict())
+    fh.close()
 
     # 2. Read package list from external command
     process = Popen(args.command.split(' '), stdout=PIPE)
     (output, err) = process.communicate()
     process.wait()
 
-    for package_info in output.splitlines():
-        m = re.match(PIP_PATTERN, package_info)
-
-        if m:
-            version = packages.get(m.group('package'))
-            if version and version < m.group('version'):
-                packages[m.group('package')] = m.group('version')
+    for package_str in output.splitlines():
+        for package_type, rule in PACKAGE_RULES.iteritems():
+            m = re.match(rule['pattern'], package_str)
+            if m:
+                package = packages.get(m.group('package'))
+                if package:
+                    version = package.get('version')
+                    if version and version > m.group('version'):
+                        package['version'] = m.group('version')
+                        print('[UPDATE] {package} to {version}'.format(**m.groupdict()))
 
     # 3. Write package list to output file
-    for package, version in packages.iteritems():
-        args.output.write('{}=={}\n'.format(package, version))
+    fh = open(args.output, 'w')
+    for package, info in packages.iteritems():
+        fh.write(PACKAGE_RULES[info['type']]['output'].format(**info) + '\n')
+    fh.close()
 
 
 def main():
